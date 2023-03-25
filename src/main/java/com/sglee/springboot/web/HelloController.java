0647c72f3e114693c085d57fef484635a856994d
isugn0125 <didong0125@naver.com>
2023-03-25 20:10:10 +0900

    lombok 플러그인 추가, dto 생성, gradle 4.10.2 변경

--- src/main/java/com/sglee/springboot/web/HelloController.java
+++ src/main/java/com/sglee/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.sglee.springboot.web;
 
+import com.sglee.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Controller JSON을 반환 @ResponseBody를 메소드마다 선언 안해도 됨.
@@ -9,5 +11,13 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(
+            @RequestParam("name") String name,
+            @RequestParam("amount") int amount
+                                     ){
+        return new HelloResponseDto(name, amount);
     }
 }
